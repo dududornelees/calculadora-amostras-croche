e9379a46642227d3ee4472522900d67e439984bd
dududornelees <eduardo.dsdorneles@gmail.com>
2022-12-30 15:29:25 -0300

    init: início do projeto

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import * as S from "./styles";
+
+export const Home = () => {
+    return (
+        <S.Container>
+            <Text>Home!</Text>
+        </S.Container>
+    );
+};
---
+++ src/routes/Routes.tsx
@@ -0,0 +1,19 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Home } from "@/pages";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export const Routes = () => {
+    return (
+        <NavigationContainer>
+            <Navigator>
+                <Screen
+                    name="Home"
+                    component={Home}
+                    options={{ headerTitleAlign: "center", headerTitle: "Calculadora de Amostras" }}
+                />
+            </Navigator>
+        </NavigationContainer>
+    );
+};
